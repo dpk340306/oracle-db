571e845796035e6e004c320fe0c01265f174fe9e
Deepak Koirala <dpk.13.jec@gmail.com>
2022-04-25 15:58:13 +0530

    Add files via upload

---
+++ scripts/CONSTRAINTS/COUNTRIES.sql
@@ -0,0 +1,7 @@
+--------------------------------------------------------
+--  Constraints for Table COUNTRIES
+--------------------------------------------------------
+
+  ALTER TABLE "HR"."COUNTRIES" ADD CONSTRAINT "COUNTRY_C_ID_PK" PRIMARY KEY ("COUNTRY_ID")
+  USING INDEX  ENABLE;
+  ALTER TABLE "HR"."COUNTRIES" MODIFY ("COUNTRY_ID" CONSTRAINT "COUNTRY_ID_NN" NOT NULL ENABLE);
---
+++ scripts/CONSTRAINTS/DEPARTMENTS.sql
@@ -0,0 +1,7 @@
+--------------------------------------------------------
+--  Constraints for Table DEPARTMENTS
+--------------------------------------------------------
+
+  ALTER TABLE "HR"."DEPARTMENTS" ADD CONSTRAINT "DEPT_ID_PK" PRIMARY KEY ("DEPARTMENT_ID")
+  USING INDEX  ENABLE;
+  ALTER TABLE "HR"."DEPARTMENTS" MODIFY ("DEPARTMENT_NAME" CONSTRAINT "DEPT_NAME_NN" NOT NULL ENABLE);
